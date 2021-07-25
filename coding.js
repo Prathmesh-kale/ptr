557329c99859cf653a3df707fb6e56e041a29526
Prathmesh-kale <79739726+Prathmesh-kale@users.noreply.github.com>
2021-07-25 11:19:43 +0530

    Add files via upload

---
+++ coding.js
@@ -0,0 +1,4 @@
+function btn()
+{
+    window.location = "index.html";
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+function coding()
+{
+    window.location = "coding.html";
+}
+
+function html()
+{
+    window.location = "html.html";
+}
+
+function css()
+{
+    window.location = "css.html";
+}
+
+function jss()
+{
+    window.location = "jss.html";
+}
